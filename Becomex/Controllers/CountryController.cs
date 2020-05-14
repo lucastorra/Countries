a2cc62d2ee4b9bbc52f621f371f59045a80fb105
lucas.torra <lucasgabrielft@hotmail.com>
2020-05-14 12:46:02 -0300

    retirando comentarios

--- Becomex/Controllers/CountryController.cs
+++ Becomex/Controllers/CountryController.cs
@@ -29,17 +29,6 @@ namespace Country.Controllers
 
         public async Task<ActionResult> ViewCountry(string alpha3Code)
         {
-            /*Fiquei com dúvida nesse ponto sobre o que seria melhor a fazer:
-             * O texto do problema diz "Faça com que a chamada no datasource não se repita em todas as chamadas da sua API."
-             * No entanto a forma que encontrei para fazer isso, não parece ser uma boa prática. (Imagino que deva ter uma melhor)
-             * Guardar a lista de países em uma propriedade estática, e realizar um Find na Lista.
-             *
-             *Country country = _countries.Find(x => x.Alpha3Code == alpha3Code);
-             * 
-             * 
-             * Por isso estou vou realizar a busca no DataSource usado o código.
-             */
-
             Models.Country country = await _countryDataSourceClient.GetCountryByCodeAsync(alpha3Code);
 
             return View(country);
